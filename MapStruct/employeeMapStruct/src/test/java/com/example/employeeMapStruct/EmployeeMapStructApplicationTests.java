8d64358764941e28eea11771ee32fe660741b4a0
rakesh <rakesh.chavan776@gmail.com>
2022-09-13 20:06:57 +0530

    spirngJPA

---
+++ MapStruct/employeeMapStruct/src/test/java/com/example/employeeMapStruct/EmployeeMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeMapStruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMapStructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
